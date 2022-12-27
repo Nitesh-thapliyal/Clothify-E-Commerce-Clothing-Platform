1891bfb1c9ac19ae9061c127c477a0478931c86d
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2022-12-27 23:11:18 +0530

    add singin controller

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { check, validationResult } = require('express-validator');
-
+var jwt = require('jsonwebtoken');
+var expressJwt = require("express-jwt");
 
 exports.signup = (req, res)=>{
 
@@ -24,6 +25,44 @@ exports.signup = (req, res)=>{
             _id: user._id
         })
     })
+};
+
+
+exports.signin = (req, res)=>{
+    const {email,password} = req.body;
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error:errors.array()[0].msg
+        });
+    }
+
+// find one match from database
+    User.findOne({email}, (err, user)=>{
+        if(err){
+            res.status(400).json({
+                error: "User email does not exists!"
+            })
+        }
+
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password do not match!"
+            })
+        }
+        //create token
+        const token = jwt.sigin({_id: user._id}, process.env.SECRET)
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+
+        // send response to frontend
+        const {_id, name, email, role} = user;
+        return res.json({token, user: {_id, name, email, role}})
+
+    })
+
+
+
 };
 
 exports.signout = (req, res)=> {
