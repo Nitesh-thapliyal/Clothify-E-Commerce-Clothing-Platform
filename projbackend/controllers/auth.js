18c7ed9d39085c3e6237f47d512faf6ff8b497fe
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2022-12-31 00:12:05 +0530

    add custom controller & protected route

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -61,9 +61,6 @@ exports.signin = (req, res)=>{
         return res.json({token, user: {_id, name, email, role}})
 
     })
-
-
-
 };
 
 exports.signout = (req, res)=> {
@@ -72,6 +69,35 @@ exports.signout = (req, res)=> {
     res.json({
         message: "User signout successfully!"
     });
-}
+};
+
+
+// Protected Routes
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+});
+
+// custom middleware bcz express jwt does not has support for isadmin and isauthenticated 
+
+exports.isAuthenticated = (req,res, next)=>{
+        let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+        if(!checker){
+            return res.status(403).json({
+                error: "ACCESS DENIED!"
+            });
+        }
+    next();
+};
+
+exports.isAdmin = (req,res, next)=>{
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error: "You are not admin!, ACCESS DENIED!"
+        });
+    }
+    next();
+};
+
 
 
