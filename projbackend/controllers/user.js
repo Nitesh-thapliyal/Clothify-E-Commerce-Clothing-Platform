f81c32ec2530a402aeb6a6ab10a7d5a8d6f81109
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-02 23:28:23 +0530

    add pushOrderInPurchaseList middleware

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -59,9 +59,38 @@ exports.userPurchaseList = (req, res)=>{
     })
 }
 
-exports.pushOrderInPurchaseList = (req, res)=>{
+exports.pushOrderInPurchaseList = (req, res, next)=>{
+    
+    let purchases = [];
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id  
+        });
+    });
 
-}
+    // store it in DB
+
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {purchases: purchases}},
+        {new: true},
+        (err, purchases)=>{
+            if(err){
+                return res.status(400).json({
+                    error: "Unable to save purchase list!"
+                })
+            }
+            next();
+        }
+    );
+    
+};
 
 
 //experiment to get all the users from the database
