86d07f818e466402be9fef3ce269bb27d3e74f71
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-02 20:42:29 +0530

    populate id and name in userpurchaselist

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,5 +1,5 @@
-const User =require("../models/user");
-
+const User = require("../models/user");
+const Order = require("../models/order");
 
 
 exports.getUserById = (req, res, next, id) =>{
@@ -45,6 +45,19 @@ exports.updateUser = (req, res) => {
     )
 }
 
+
+exports.userPurchaseList = (req, res)=>{
+    Order.find({user:req.profile._id})
+    .populate("user", "_id name")
+    .exec((err, order)=>{
+        if(err){
+            return res.status(400).json({
+                error: "No order in this account!"
+            })
+        }
+        return res.json(order);
+    })
+}
 
 
 
