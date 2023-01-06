762bbd40812f22bf5c689c6c8cf83c0639672a5d
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-06 23:02:34 +0530

    update inventory stock and sold

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -151,3 +151,26 @@ exports.getAllProducts = (req, res)=>{
     res.json(products);
   })
 }
+
+
+// increase the sold and decrease the stock
+exports.updateStock = (req,res, next)=>{
+
+  let myOperations = req.body.order.products.map(prod => {
+    return {
+      updateOne: {
+        filter: {_id: prod._id},
+        update: {$inc: {stock: -prod.count, sold: +prod.count}}
+      }
+    }
+  })
+
+  Product.bulkWrite(myOperations,{}, (err, products)=>{
+    if(err){
+      return res.status(400).json({
+        error: "Bulk operations failed!"
+      })
+    }
+    next();
+  });
+};
