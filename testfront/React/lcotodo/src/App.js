34afbec41070616d502325fffa24e6e8f0d877a9
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-09 19:12:38 +0530

    todo update

--- testfront/React/lcotodo/src/App.js
+++ testfront/React/lcotodo/src/App.js
@@ -5,7 +5,40 @@ import "./App.css";
 
 class App extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    }
+  }
 
+  addItem(todoValue){
+    if(todoValue !== ""){
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItemL: ""
+      });
+    }
+  }
+
+deleteItem(id){
+  const list = [...this.state.list];
+  const updatedlist = list.filter(item=>item.id !==id);
+  this.setState({list: updatedlist})
+}
+
+updateInput(input){
+  this.setState({newItem: input});
+}
   
   render(){
     return (
@@ -19,11 +52,35 @@ class App extends React.Component{
           type="text"
           className="input-text"
           placeholder="Write a ToDo"
+          required
+          value = {this.state.newItem}
+          onChange = {e => this.updateInput(e.target.value)}
           />
 
-          <button className="add-btn">Add ToDo</button>
+          <button
+          className="add-btn"
+          onClick = {()=>this.addItem(this.state.newItem)}
+          disabled = {!this.state.newItem.length}
+          >Add ToDo</button>
           <div className="list">
             <ul>
+              {this.state.list.map(item => {
+                return(
+                  <li key={item.id}>
+                      <input 
+                      type="checkbox"
+                      name="isDone"
+                      checked={item.isDone}
+                      onChange={()=>{}}
+                    />
+                    {item.value}
+                    <button
+                    className = "btn"
+                    onClick = {()=> this.deleteItem(item.id)}
+                    >Delete</button>
+                  </li>
+                );
+              })}
               <li>
                 <input type="checkbox" name=""></input>
                 Record Youtube Videos
