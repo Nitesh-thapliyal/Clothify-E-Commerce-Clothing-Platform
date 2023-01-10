476dfd6df918b36959a9edf66d6587ea4a7aa9a3
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-11 00:34:52 +0530

    add base,api,cdn css

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import "../styles.css"
+import {API} from "../backend"
+import Base from "./Base"
 
 export default function App(){
+  console.log("API IS:", API);
   return(
-    <div>
-      <h1>Hello FrontEnd!!</h1>
-    </div>
+    <Base>
+      <h1 className="text-white">Hello front end!!</h1>
+    </Base>
   )
 }
