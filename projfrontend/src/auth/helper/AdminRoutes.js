d3fb6fcc3a96b6d6504cd1b401f53bfda04a146e
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-13 13:03:05 +0530

    add private and admin route

---
+++ projfrontend/src/auth/helper/AdminRoutes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Route, Redirect} from "react-router-dom";
+import { isAuthenticated } from "./index";
+
+const AdminRoute = ({component: Component,  ...rest}) => {
+    return(
+        <Route
+            {...rest}
+            render = { props =>
+                isAuthenticated() && isAuthenticated().user.roll === 1 ? (
+                    <Component {...props}/>
+                ) : (
+                    <Redirect
+                    
+                        to = {{
+                            pathname: "/signin",
+                            state: {from: props.location}
+                        }}
+                    
+                    />
+                )
+        }
+        />
+    )
+};
+
+
+export default AdminRoute;
