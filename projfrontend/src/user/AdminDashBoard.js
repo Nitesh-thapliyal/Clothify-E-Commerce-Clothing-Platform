c1b0541b95a70916e40bec03507eb18875d5c9a4
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-14 00:25:37 +0530

    add right admin pannel

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,12 +1,48 @@
 import React from "react";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper/index";
+import { Link } from "react-router-dom";
 
 
 const AdminDashboard = () => {
-    return(
-        <Base title="Profile Page">
-            <h1>This is Admin Dashboard</h1>
 
+    const {user: {name, email, role}    
+    }  = isAuthenticated()
+
+    const adminLeftSide = () => {
+        return(
+            <div className="card">
+                <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <Link to="/admin/create/category" className="nav-link text-success">Create Categories</Link>                        
+                        <Link to="/admin/create/product" className="nav-link text-success">Create Product</Link>                        
+                        <Link to="/admin/products" className="nav-link text-success">Manage Products</Link>                        
+                        <Link to="/admin/orders" className="nav-link text-success">Manage Orders</Link>                        
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+    const adminRightSide = () => {
+        return(
+            <div>
+                <h1>Hii Admin...</h1>
+            </div>
+        )
+    }
+
+    return(
+        <Base title="Welcome to Admin area" description="Manage all of your products here" className="container bg-success p-4">
+        <div className="row">
+            <div className="col-3">
+                {adminLeftSide()}
+            </div>
+            <div className="col-9">
+                {adminRightSide()}
+            </div>
+        </div>
         </Base>
     )
 }
