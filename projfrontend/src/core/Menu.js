8344fa1cdd933819602dd96978a0a9d31b34c6e8
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-13 11:49:32 +0530

    fix signout and signin visibility

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link, withRouter} from "react-router-dom";
 import {signout, isAuthenticated} from "../auth/helper"
 
@@ -54,25 +54,29 @@ const Menu = ({history}) =>(
                 </Link>
             </li>
 
-            <li className="nav-item">
-                <Link 
-                    style={currentTab(history, "/signup")}
-                    className="nav-link" to="/signup"
-                >
-                    Signup
-
-                </Link>
-            </li>
-
-            <li className="nav-item">
-                <Link 
-                    style={currentTab(history, "/signin")}
-                    className="nav-link" to="/signin"
-                >
-                    Signin
-
-                </Link>
-            </li>
+        {!isAuthenticated() && (
+                    <Fragment>
+                    <li className="nav-item">
+                        <Link 
+                            style={currentTab(history, "/signup")}
+                            className="nav-link" to="/signup"
+                        >
+                            Signup
+        
+                        </Link>
+                    </li>
+        
+                    <li className="nav-item">
+                        <Link 
+                            style={currentTab(history, "/signin")}
+                            className="nav-link" to="/signin"
+                        >
+                            Signin
+        
+                        </Link>
+                    </li>
+                </Fragment>
+        )}
 
           {isAuthenticated() && (
               <li className="nav-item">
