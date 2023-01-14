71d329a2ff7d3727ab84a2bad4319d7cf63fe378
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-14 23:37:50 +0530

    add admin addCategory

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -7,6 +7,8 @@ import AdminRoute from './auth/helper/AdminRoutes';
 import PrivateRoute from './auth/helper/PrivateRoutes';
 import UserDashboard from './user/UserDashBoard'
 import AdminDashboard from './user/AdminDashBoard'
+import AddCategory from './admin/AddCategory';
+
 
 const Router = () =>{
   return(
@@ -17,6 +19,7 @@ const Router = () =>{
         <Route path='/signin' exact component={Signin}/>
         <PrivateRoute path='/user/dashboard' exact component={UserDashboard}/>
         <AdminRoute path='/admin/dashboard' exact component={AdminDashboard}/>
+        <AdminRoute path='/admin/create/category' exact component={AddCategory}/>
       </Switch>    
     </BrowserRouter>
   );
---
+++ projfrontend/src/admin/AddCategory.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Base from "../core/Base";
+
+
+const AddCategory = () =>{
+   return(
+        <Base title="Create a category" description="Add a new category" className="container bg-info p-4">
+            <div className="row bg-white rounded">
+                <div className="col-md-8 offset-md-2">
+                        <h1>Hello</h1>
+                </div>
+            </div>
+
+        </Base>
+   );
+};
+
+
+export default AddCategory;
