10dee3b0dbe5d443ebe06a592a159c3d28d6ba4a
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-15 15:19:02 +0530

    connect adminp create catefory to backend

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -2,12 +2,12 @@ import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
-
+import {createCategory} from "./helper/adminapicall"
 
 
 const AddCategory = () =>{
 
-    const [name, setName] = useState("initialState")
+    const [name, setName] = useState("")
     const [error, setError] = useState(false)
     const [success, setSuccess] = useState(false)
     
@@ -22,12 +22,48 @@ const AddCategory = () =>{
         )
     }
 
+    const handleChange = (event) => {
+        setError("");
+        setName(event.target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setError("");
+        setSuccess(false);
+
+        //backend request fire
+        createCategory(user._id, token, {name})
+        .then(data => {
+            if(data.error){
+                setError(true)
+            }
+            else{
+                setError("");
+                setSuccess(true);
+                setName("");
+            }
+        })
+    }
+
+    const successMessage = () => {
+        if(success){
+            return <h4 className="text-success">Category created scuccessfully</h4>
+        }
+    }
+
+    const warningMessage = () => {
+        if(error){
+            return <h4 className="text-success">Failed to create category</h4>
+        }
+    }
+
     const myCategoryForm = () => (
         <form>
             <div className="form-group">
                 <p className="lead">Enter the category</p>
-                <input type="text" className="form-control my-3" autoFocus required placeholder="For ex. Summer" />
-                <button className="btn btn-outline-info mb-2">Create Category</button>
+                <input type="text" className="form-control my-3" onChange={handleChange} value= {name} autoFocus required placeholder="For ex. Summer" />
+                <button onClick={onSubmit} className="btn btn-outline-info mb-2">Create Category</button>
             </div>
         </form>
     );
@@ -36,6 +72,8 @@ const AddCategory = () =>{
         <Base title="Create a category" description="Add a new category" className="container bg-info p-4">
             <div className="row bg-white rounded">
                 <div className="col-md-8 offset-md-2">
+                    {successMessage()}
+                    {warningMessage()}
                     {myCategoryForm()}
                     {goBack()}
                 </div>
