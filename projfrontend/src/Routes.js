6beebc26289436cd55a2f820e417c9fbc11e54de
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-20 18:22:25 +0530

    add delete product

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -10,6 +10,7 @@ import AdminDashboard from './user/AdminDashBoard'
 import AddCategory from './admin/AddCategory';
 import ManageCategories from './admin/ManageCategories';
 import AddProduct from './admin/AddProduct';
+import ManageProducts from './admin/ManageProducts';
 
 
 const Router = () =>{
@@ -24,6 +25,7 @@ const Router = () =>{
         <AdminRoute path='/admin/dashboard' exact component={AdminDashboard}/>
         <AdminRoute path='/admin/create/category' exact component={AddCategory}/>
         <AdminRoute path='/admin/categories' exact component={ManageCategories}/>
+        <AdminRoute path='/admin/products' exact component={ManageProducts}/>
         <AdminRoute path='/admin/create/product' exact component={AddProduct}/>
       </Switch>    
     </BrowserRouter>
