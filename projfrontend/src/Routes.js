c1419032014b55bbcba1ce10ca8fad8d2ce5aef5
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-21 20:17:46 +0530

    add update product

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -11,7 +11,7 @@ import AddCategory from './admin/AddCategory';
 import ManageCategories from './admin/ManageCategories';
 import AddProduct from './admin/AddProduct';
 import ManageProducts from './admin/ManageProducts';
-
+import UpdateProduct from './admin/UpdateProduct';
 
 const Router = () =>{
   return(
@@ -26,6 +26,7 @@ const Router = () =>{
         <AdminRoute path='/admin/create/category' exact component={AddCategory}/>
         <AdminRoute path='/admin/categories' exact component={ManageCategories}/>
         <AdminRoute path='/admin/products' exact component={ManageProducts}/>
+        <AdminRoute path='/admin/product/update/:productId' exact component={UpdateProduct}/>
         <AdminRoute path='/admin/create/product' exact component={AddProduct}/>
       </Switch>    
     </BrowserRouter>
