7952d75da1c29d5ab2fe44f1d95d270dd7cf7e49
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-23 13:46:03 +0530

    update home page

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,14 +2,18 @@ import React from 'react';
 import "../styles.css"
 import {API} from "../backend"
 import Base from "./Base"
+import Card from './Card';
+
 
 export default function App(){
-  console.log("API IS:", API);
+  
+  
+
   return(
     <Base title='Home Page' description='Welcome to the Store'>
-      <div className='row'>
+      <div className='row text-center'>
         <div className='col-4'>
-            <button className='btn btn-success'>Test</button>
+            <Card/>
         </div>
 
         <div className='col-4'>
