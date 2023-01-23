4faf6b10ac9c87b6aa5498954787a952657fb55d
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2023-01-23 15:45:45 +0530

    add rendering of homepage card button

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ImageHelper from "./helper/ImageHelper";
 
-const Card = ({
-    product
-}) => {
+const Card = ({product,addtoCart = true,removeFromCart = false}) => {
+
+
+    const showAddToCart = (addtoCart) => {
+        return(
+            addtoCart && (
+                <button
+                onClick={() => {}}
+                className="btn btn-block btn-outline-success mt-2 mb-2"
+              >
+                Add to Cart
+              </button>
+            )
+        )
+    }
+
+    const showRemoveFromCart = (removeFromCart) => {
+        return(
+            removeFromCart && (
+                <button
+                onClick={() => {}}
+                className="btn btn-block btn-outline-danger mt-2 mb-2"
+              >
+                Remove from cart
+              </button>
+            )
+        )
+    }
+
     return (
       <div className="card text-white bg-dark border border-info ">
         <div className="card-header lead">A photo from pexels</div>
@@ -14,22 +40,8 @@ const Card = ({
           </p>
           <p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
           <div className="row">
-            <div className="col-12">
-              <button
-                onClick={() => {}}
-                className="btn btn-block btn-outline-success mt-2 mb-2"
-              >
-                Add to Cart
-              </button>
-            </div>
-            <div className="col-12">
-              <button
-                onClick={() => {}}
-                className="btn btn-block btn-outline-danger mt-2 mb-2"
-              >
-                Remove from cart
-              </button>
-            </div>
+            <div className="col-12">{showAddToCart(addtoCart)}</div>
+            <div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
           </div>
         </div>
       </div>
